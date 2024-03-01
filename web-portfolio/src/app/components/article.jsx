b975e631683fe2320929a1f6c14327216af06703
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-03-01 15:48:45 -0300

    creacion de article y footer

--- web-portfolio/src/app/components/article.jsx
+++ web-portfolio/src/app/components/article.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Article() {
   return (
-    <main className="p-4">
+    <main className="p-4 justify-center ">
       {/* el elemento padre article ajusta el ancho de la tarjeta  */}
       {/* div contenedor  */}
-      <div className="grid grid-cols-4 " >
-        <article className="bg-white shadow rounded overflow-hidden flex flex-col ">
-            {/* header  */}
-          <div className="h-72 ">
+      <div className="mx-auto grid gap-4 max-w-6xl md:grid-cols-2  md:items-center lg:grid-cols-3 ">
+        <article className="flex flex-col bg-white shadow rounded overflow-hidden">
+          {/* header  */}
+          <div className="h-52 ">
             <Image
               className="h-full w-full object-cover object-center "
               src="/img/2.jpeg"
@@ -18,17 +18,20 @@ export default function Article() {
             />
           </div>
           {/* contenido  */}
-          <div className="p-5  flex-1 space-y-5 ">
+          <div className="p-5 flex-1 space-y-3 ">
             <h3 className="text-sm font-semibold text-slate-900">Laravel</h3>
             <h2 className="text-xl font-semibold text-slate-800 leading-tight">
               Desarrollo de una API con Laravel y un par de cositas mas que
               estan una masa
             </h2>
-            <p className="text-slate-500">
-              Omnis
-              culpa eveniet sunt totam? Aspernatur eaque perspiciatis, illo
-              fugiat alias quod impedit temporibus incidunt eos tempore totam.
-              Minima neque sunt sit?
+            <p className="text-slate-500 hidden md:block">
+              Omnis culpa eveniet sunt totam? Aspernatur eaque perspiciatis,
+              illo fugiat alias quod impedit temporibus incidunt eos tempore
+              alias quod impedit temporibus incidunt eos tempore Omnis culpa
+              eveniet sunt totam? Aspernatur eaque perspiciatis, Omnis culpa
+              eveniet sunt totam? Aspernatur eaque perspiciatis, illo fugiat
+              alias quod impedit temporibus incidunt eos tempore totam. Minima
+              neque sunt sit?
             </p>
           </div>
           {/* fotter  */}
@@ -52,8 +55,8 @@ export default function Article() {
           </div>
         </article>
 
-        <article className="bg-white shadow rounded overflow-hidden ">
-          <div className="h-72 ">
+        <article className="flex flex-col bg-white shadow rounded overflow-hidden  ">
+          <div className="h-52 ">
             <Image
               className="h-full w-full object-cover object-center "
               src="/img/4.jpeg"
@@ -62,17 +65,20 @@ export default function Article() {
               alt="Picture of the author"
             />
           </div>
-          <div className="p-5 space-y-5 ">
+          <div className="p-5 space-y-5  flex-1">
             <h3 className="text-sm font-semibold text-slate-900">Laravel</h3>
             <h2 className="text-xl font-semibold text-slate-800 leading-tight">
               Desarrollo de una API con Laravel y un par de cositas mas que
               estan una masa
             </h2>
-            <p className="text-slate-500">
+            <p className="text-slate-500  hidden md:block ">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+              culpa eveniet sunt totam? Aspernatur eaque perspiciatis, ialias
+              quod impedit temporibus incidunt eos tempore fugiat ali poreOmnis
               culpa eveniet sunt totam? Aspernatur eaque perspiciatis, illo
-              fugiat alias quod impedit temporibus incidunt eos tempore totam.
-              Minima neque sunt sit?
+              fugiat alias quod impedit temporibus incidunt eos tempore fugiat
+              alias quod impedit temporibus incidunt eos tempore totam. Minima
+              neque sunt sit?
             </p>
           </div>
           {/* fotter  */}
@@ -96,8 +102,8 @@ export default function Article() {
           </div>
         </article>
 
-        <article className="bg-white shadow rounded overflow-hidden ">
-          <div className="h-72 ">
+        <article className="flex flex-col bg-white shadow rounded overflow-hidden  ">
+          <div className="h-52 ">
             <Image
               className="h-full w-full object-cover object-center "
               src="/img/3.jpeg"
@@ -106,17 +112,24 @@ export default function Article() {
               alt="Picture of the author"
             />
           </div>
-          <div className="p-5 space-y-5 ">
+          <div className="p-5 space-y-5 flex-1 ">
             <h3 className="text-sm font-semibold text-slate-900">Laravel</h3>
             <h2 className="text-xl font-semibold text-slate-800 leading-tight">
               Desarrollo de una API con Laravel y un par de cositas mas que
               estan una masa
             </h2>
-            <p className="text-slate-500">
+            <p className="text-slate-500  hidden md:block">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              culpa eveniet sunt totam? Aspernatur eaque perspiciatis, illo
-              fugiat alias quod impedit temporibus incidunt eos tempore totam.
-              Minima neque sunt sit?
+              culpa eveniet sunt totam? Aspernatur eaque perspiciatis,
+              illoporeOmnis culpa eveniet sunt totam? Aspernatur eaque
+              perspiciatis, illo fugiat alias quod impedit temporibus incidunt
+              eos tempore fugiat aliporeOmnis culpa eveniet sunt totam?
+              Aspernatur eaque perspiciatis, illo fugiat alias quod impedit
+              temporibus incidunt eos tempore fugiat aliporeOmnis culpa eveniet
+              sunt totam? Aspernatur eaque perspiciatis, illo fugiat alias quod
+              impedit temporibus incidunt eos tempore fugiat ali fugiat alias
+              quod impedit temporibus incidunt eos tempore totam. Minima neque
+              sunt sit?
             </p>
           </div>
           {/* fotter  */}
@@ -140,8 +153,8 @@ export default function Article() {
           </div>
         </article>
 
-        <article className="bg-white shadow rounded overflow-hidden ">
-          <div className="h-72 ">
+        <article className="flex flex-col bg-white shadow rounded overflow-hidden  ">
+          <div className="h-52 ">
             <Image
               className="h-full w-full object-cover object-center "
               src="/img/4.jpeg"
@@ -150,13 +163,13 @@ export default function Article() {
               alt="Picture of the author"
             />
           </div>
-          <div className="p-5 space-y-5 ">
+          <div className="p-5  space-y-5  flex-1">
             <h3 className="text-sm font-semibold text-slate-900">Laravel</h3>
             <h2 className="text-xl font-semibold text-slate-800 leading-tight">
               Desarrollo de una API con Laravel y un par de cositas mas que
               estan una masa
             </h2>
-            <p className="text-slate-500">
+            <p className="text-slate-500  hidden md:block ">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
               culpa eveniet sunt totam? Aspernatur eaque perspiciatis, illo
               fugiat alias quod impedit temporibus incidunt eos tempore totam.
--- web-portfolio/src/app/page.js
+++ web-portfolio/src/app/page.js
@@ -1,11 +1,13 @@
 import Article from "./components/article";
 import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Article />
+      <Footer/>      
     </main>
   );
 }
