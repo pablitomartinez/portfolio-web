71c415e806348947ccd2c24cd8b384924c36c4d1
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-03-04 01:33:21 -0300

    contact estructura

--- web-portfolio/src/app/contact/page.js
+++ web-portfolio/src/app/contact/page.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      soy contact
+    <div className="flex  flex-col p-4 space-y-2">
+      <div className="font-semibold">Want to work with me?</div>
+      {/* form  */}
+      <div className="flex flex-col items-center w-full">
+        <form>
+          <div className="t-10 grid grid-cols-1">
+            <div className="sm:col-span-4">
+              <label
+                for="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    className="block border-0 border-b-2  bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Nombre"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="t-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                for="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Nombre"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="t-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                for="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Nombre"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <input
+            type="email"
+            name="floating_email"
+            id="floating_email"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder="aqui "
+            required
+          />
+
+          <button
+            type="submit"
+            className="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            GET IN TOUCH
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
